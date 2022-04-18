dee9216fb6a99b04da0bb406309dbc0b5681f71f
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-04-18 13:26:46 +0100

    Project link component.

---
+++ src/components/elements/ProjectLink.tsx
@@ -0,0 +1,31 @@
+
+import React, { useContext, ReactNode } from 'react'
+import styled from 'styled-components'
+import { ThemeContext } from '../../context/Theme'
+
+const ProjectLinkButton = styled.a`
+	display: grid;
+	place-items: center;
+	text-decoration: none;
+	font-weight: 700;
+	color: ${props => props.theme.background};
+	background-color: ${props => props.theme.accent};
+	border-radius: 100vw;
+	width: 7rem;
+	height: 2.75rem;
+
+	&:hover,
+	&:focus {
+		color: ${props => props.theme.background};
+		background-color: ${props => props.theme.primary};
+	}
+`
+
+export default function ProjectLink({ projectLink }: { projectLink: string }) {
+	const theme = useContext(ThemeContext)
+	return (
+		<ProjectLinkButton theme={theme} href={projectLink} target='_blank'>
+			Visit Site
+		</ProjectLinkButton>
+	)
+}
