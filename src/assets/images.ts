a45c79a62fe148010db7529d48c58c3480893bbc
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-07-04 19:42:31 +0100

    Update build config and asset handling.

---
+++ src/assets/images.ts
@@ -0,0 +1,21 @@
+import mugshot from './images/mugshot.jpg'
+import newcastleGoClubDesktop from './images/newcastle-go-club_desktop.png'
+import newcastleGoClubMobile from './images/newcastle-go-club_mobile.png'
+import socialMediaDashboardDesktop from './images/social-media-dashboard_desktop.png'
+import socialMediaDaskboardMobile from './images/social-media-dashboard_mobile.png'
+import spaceXLaunchScheduleDesktop from './images/spacex-launch-schedule_desktop.png'
+import spaceXLaunchScheduleMobile from './images/spacex-launch-schedule_mobile.png'
+import stylestageSpectrumDesktop from './images/stylestage-spectrum_desktop.png'
+import stylestageSpectrumMobile from './images/stylestage-spectrum_mobile.png'
+
+export {
+  mugshot,
+  newcastleGoClubDesktop,
+  newcastleGoClubMobile,
+  socialMediaDashboardDesktop,
+  socialMediaDaskboardMobile,
+  spaceXLaunchScheduleDesktop,
+  spaceXLaunchScheduleMobile,
+  stylestageSpectrumDesktop,
+  stylestageSpectrumMobile
+}
